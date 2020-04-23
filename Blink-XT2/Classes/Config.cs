7d92e70a89ac8b4eae4f7bfcf0c9ba3a36ae718a
UweR70 <UweRaatz@gmx.de>
2020-04-23 15:12:27 +0200

    (Help) text handling fine tuning.

--- Blink-XT2/Classes/Config.cs
+++ Blink-XT2/Classes/Config.cs
@@ -8,6 +8,9 @@
         public readonly static string TitleAppNameAndVersion = $"{AppName} {AppVersion}";
         public readonly static string TitleErrorAppNameAndVersion = $"{TitleAppNameAndVersion} - Error";
 
+        // $"{DefaultRootStoragePart}\{StoragePart}\NetworkName\Camera[0].Name\" ...
+        //             C:\Temp       \ Blink_XT2   \HomeSweetHome\Garage      \  ...
+        public const string DefaultRootStoragePart = @"C:\Temp";
         public const string StoragePart = "Blink_XT2";
     }
 }
